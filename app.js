b712044ed95517e83d83da2f40ec83a2f3697412
Sauvik Chakraborty <sc@email>
2020-03-22 16:44:39 -0400

    minor rectifications

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ class Jeopardy {
 		this.players = [ p1, p2, p3 ];
 		this.height = height;
 		this.width = width;
-		this.arr = [];
 		this.ids = [];
 		this.clues = [];
 		this.board = [];
 		this.makeBoard();
 		this.makeHTMLBoard();
+		this.initQuestions();
 		this.gameover = false;
-		this.getCategory();
 		this.lockboard = false;
 	}
 
@@ -45,11 +44,8 @@ class Jeopardy {
 			for (let x = 0; x < this.width; x++) {
 				const cell = document.createElement('td');
 				cell.setAttribute('id', `${y}-${x}`);
+				cell.classList.add('cell');
 				cell.innerText = '$' + 200 * (y + 1);
-				cell.style.textAlign = 'center';
-				cell.style.fontFamily = 'Impact';
-				cell.style.fontSize = '90px';
-				cell.style.color = 'darkorange';
 				row.append(cell);
 			}
 
@@ -61,27 +57,38 @@ class Jeopardy {
 		board.addEventListener('click', this.handleEachClick);
 	}
 
+	async initQuestions() {
+		this.arr = await this.getCategory();
+		const ids = await this.getCategoryId(this.arr);
+		this.getClues(ids);
+		// and continue with this.ids = await getCategoriesIds
+		// etc
+	}
+
 	//Obtains categories and pushes the ids of those categories to arr property of new object
 	async getCategory() {
+		// isn't it more like setCategory? check out object attibutes setters https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/set
 		//getting 100 categories from Jeopardy API
-		const result = await axios.get('http://jservice.io/api/categories?count=100');
-
+		const result = await axios.get('http://jservice.io/api/categories?count=100'); // is it neccessary to get 100 when number of categories is already defined?
+		const categoryIds = [];
 		for (let x = 0; x < this.width; x++) {
 			const random = Math.floor(Math.random() * result.data.length);
-			const topmostCell = document.getElementById(`${x}`);
-			this.arr.push(result.data[random].id);
-			topmostCell.innerText = result.data[random].title.toUpperCase();
-			topmostCell.style.textAlign = 'center';
-			topmostCell.style.fontFamily = 'Impact';
-			topmostCell.style.fontSize = '40px';
-			topmostCell.style.color = 'white';
+			categoryIds.push(result.data[random].id);
+			this.topMostRowStyling(x, random, result);
 		}
-		console.log(this.arr);
-		return this.getCategoryId(this.arr);
+		// i would rather return the whole object not just ids and then execute the six lines above in initQuestions() and loop over arr. The reason is that this method should do just one part of the logic - get categories and thats it.
+		console.log(categoryIds);
+		return categoryIds;
+	}
+
+	topMostRowStyling(x, randNum, res) {
+		const topmostCell = document.getElementById(`${x}`);
+		topmostCell.innerText = res.data[randNum].title.toUpperCase();
+		topmostCell.classList.add('topmostcell');
 	}
 	//gets ids of categories and invokes getClues instance method to obtain clues
 	async getCategoryId(arr) {
-		const res1 = await axios.get(`http://jservice.io/api/category?id=${arr[0]}`);
+		const res1 = await axios.get(`http://jservice.io/api/category?id=${arr[0]}`); // number of calls can vary right? You should handle the number of api calls dynamically. Take a look at Promise.all() https://alligator.io/js/async-functions/
 		const res2 = await axios.get(`http://jservice.io/api/category?id=${arr[1]}`);
 		const res3 = await axios.get(`http://jservice.io/api/category?id=${arr[2]}`);
 		const res4 = await axios.get(`http://jservice.io/api/category?id=${arr[3]}`);
@@ -90,11 +97,10 @@ class Jeopardy {
 
 		this.ids.push(res1.data, res2.data, res3.data, res4.data, res5.data, res6.data);
 		console.log(this.ids);
-		return this.getClues(this.ids);
+		return this.ids;
 	}
 	//gets clues based on the ids of the categories
 	getClues(arr) {
-		//need to find a better replacement for 'element' below
 		for (let element of arr) {
 			this.clues.push(element.clues);
 		}
@@ -108,11 +114,18 @@ class Jeopardy {
 
 		for (let x = 0; x < this.width; x++) {
 			for (let y = 0; y < this.height - 1; y++) {
+				//Checking to see if the clicked td matches the id of a td within board
 				if (targetID.includes(`${y}-${x}`) && targetID) {
-					this.lockboard = true;
+					//Creating a new div for the question pertaining to the clicked td
+					let newDiv = document.createElement('DIV');
 					let clickedTD = document.getElementById(`${y}-${x}`);
-					console.log(clickedTD);
-					clickedTD.classList.add('zoom-in');
+
+					//Setting the innerText of the new div to the relevant question
+					newDiv.innerText = this.clues[x][y].question;
+					//Check css for zoom-in animation. The new div does not animate to fill the whole screen
+					newDiv.classList.add('zoom-in');
+
+					console.log(newDiv);
 					console.log(this.clues[x][y].question);
 					console.log(this.clues[x][y].answer);
 				}
@@ -121,4 +134,4 @@ class Jeopardy {
 	}
 }
 
-let newGame = new Jeopardy('Sauvik', 'Bethany', 'Ez');
+let newGame = new Jeopardy('P1', 'P2', 'P3');
